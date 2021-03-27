53e543d9a6d869da95fb5dd69aad483568501e7d
Damian Cur <damian.cur91@gmail.com>
2021-03-27 19:22:40 +0100

    declared varriables

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
 // http://websamuraj.pl/examples/js/projekt11/
+
+const startBtn = document.querySelector(".main");
+const resetBtn = document.querySelector(".reset");
+const time = document.querySelector(".time div");
