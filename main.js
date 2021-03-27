519ff8087a4d00bee0d573ebc22078da4ff387c2
Damian Cur <damian.cur91@gmail.com>
2021-03-27 21:36:26 +0100

    added second counting function, seconds function is bugged

--- main.js
+++ main.js
@@ -6,22 +6,41 @@ const time = document.querySelector(".time div");
 
 
 
+const startTimer = () => {
+    const currentTime = new Date().getTime();
+    let startTime = setInterval(seconds, 10);
+
+    function seconds() {
+        let newTime = new Date().getTime();
+        // const stopwatchTime = newTime - currentTime;
+
+        let seconds = Math.floor((newTime / 1000) - (currentTime / 1000));
+
+        let hundreths = Math.floor((newTime) - (currentTime));
+        time.textContent = `${seconds}:${hundreths}`;
+
+        console.log(hundreths);
+    }
+
+
+//ZROBIC KOLEJNĄ FUNKCJĘ, KTÓRA BĘDZIE ODPOWIADAŁA ZA SETNE SEKUNDY, USTAWIĆ INTERWAŁ, PRZEKAZANIE DANYCH DO TIME.TEXTCONTENT ORAZ ZROBIĆ ZATRZYMANIE INTERWAŁU
 
 
 
+    if (startBtn.textContent === "Start") {
+        startBtn.textContent = "Stop"
 
+    } else if (startBtn.textContent === "Stop") {
+        startBtn.textContent = "Start";
 
+    }
 
-
-
-
-
-
+}
 
 const resetTimer = () => {
     time.textContent = "---"
 }
 
 
-
+startBtn.addEventListener("click", startTimer);
 resetBtn.addEventListener("click", resetTimer);
