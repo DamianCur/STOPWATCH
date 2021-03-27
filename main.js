93c47dbaff838128727ba9fe456592c4d744c89b
Damian Cur <damian.cur91@gmail.com>
2021-03-27 19:24:57 +0100

    added reset feature

--- main.js
+++ main.js
@@ -3,3 +3,25 @@
 const startBtn = document.querySelector(".main");
 const resetBtn = document.querySelector(".reset");
 const time = document.querySelector(".time div");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const resetTimer = () => {
+    time.textContent = "---"
+}
+
+
+
+resetBtn.addEventListener("click", resetTimer);
