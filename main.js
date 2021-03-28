22a14fe11384502bf8979c5316ffd6cc5a025196
Damian Cur <damian.cur91@gmail.com>
2021-03-28 14:14:38 +0200

    added function start and stop interval, restart function is bugged

--- main.js
+++ main.js
@@ -4,36 +4,48 @@ const startBtn = document.querySelector(".main");
 const resetBtn = document.querySelector(".reset");
 const time = document.querySelector(".time div");
 
-
-
 const startTimer = () => {
     const currentTime = new Date().getTime();
-    let startTime = setInterval(seconds, 10);
 
-    function seconds() {
+
+    if (startBtn.textContent === "Stop") {
+        startBtn.textContent = "Start";
+        clearInterval(startTime);
+        
+
+    } else if (startBtn.textContent === "Start") {
+        startBtn.textContent = "Stop";
+        startTime = setInterval(timer, 10);
+        
+    }
+
+
+
+
+    function timer() {
+        
         let newTime = new Date().getTime();
         // const stopwatchTime = newTime - currentTime;
 
-        let seconds = Math.floor((newTime / 1000) - (currentTime / 1000));
+        seconds = Math.floor((newTime / 1000) - (currentTime / 1000));
 
-        let hundreths = Math.floor((newTime) - (currentTime));
+        hundreths = Math.floor((newTime / 10) - (currentTime / 10)) % 100;
+
+    
+
+        if (hundreths < 10) {
+            hundreths = `0${hundreths}`;
+        }
+
         time.textContent = `${seconds}:${hundreths}`;
-
-        console.log(hundreths);
     }
 
 
-//ZROBIC KOLEJNĄ FUNKCJĘ, KTÓRA BĘDZIE ODPOWIADAŁA ZA SETNE SEKUNDY, USTAWIĆ INTERWAŁ, PRZEKAZANIE DANYCH DO TIME.TEXTCONTENT ORAZ ZROBIĆ ZATRZYMANIE INTERWAŁU
+    //ZROBIC KOLEJNĄ FUNKCJĘ, KTÓRA BĘDZIE ODPOWIADAŁA ZA SETNE SEKUNDY, USTAWIĆ INTERWAŁ, PRZEKAZANIE DANYCH DO TIME.TEXTCONTENT ORAZ ZROBIĆ ZATRZYMANIE INTERWAŁU
 
 
 
-    if (startBtn.textContent === "Start") {
-        startBtn.textContent = "Stop"
 
-    } else if (startBtn.textContent === "Stop") {
-        startBtn.textContent = "Start";
-
-    }
 
 }
 
